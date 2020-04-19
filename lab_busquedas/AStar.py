e934435f4226fc03671cc8a42dd7bddb09fb014c
Juan Flores <jgonzalofh@hotmail.com>
2020-04-19 18:44:55 -0500

    Se ordeno las carpetas para compartir el repositorio.

--- lab_busquedas/AStar.py
+++ lab_busquedas/AStar.py
@@ -2,33 +2,35 @@
 from queue import PriorityQueue
 
 import networkx as nx
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import random
 import math
 
+
 class PathSearch(object):
     X = nx.Graph()
-    nodes_dict= {}
+    nodes_dict = {}
     search_label = 0
     Helper = 0
     START = 0
     GOAL = 0
-    
-    #Creational methods:
-    def __AddNodes(self,quantity):        
-        #Grid-like with uniform spaces 
-        #position varies dependent of 'height' variable
+
+    # Creational methods:
+    def __AddNodes(self, quantity):
+        # Grid-like with uniform spaces
+        # position varies dependent of 'height' variable
         last_node = 0
         height = 0
-        for i in range(0,quantity):
-            for j in range(0,quantity):
-                self.nodes_dict[j+last_node] = (j,height)#(height,j) #orientation
+        for i in range(0, quantity):
+            for j in range(0, quantity):
+                self.nodes_dict[j + last_node] = (j, height)  # (height,j) #orientation
             last_node = last_node + quantity
-            height = height +1
+            height = height + 1
         self.X.add_nodes_from(self.nodes_dict.keys())
-        return    
-    def __AddEdges(self,quantity):
-        #Bilateral Edges
+        return
+
+    def __AddEdges(self, quantity):
+        # Bilateral Edges
         for i in self.X.nodes:
             for j in range(0, 3):
                 # Verifie existence of node
@@ -41,14 +43,15 @@ class PathSearch(object):
                 if self.X.has_node(i - quantity + 1) and (i - quantity + 1) % quantity is not 0:
                     self.X.add_edge(i, i - quantity + 1, color='silver')  # from i to i-quantity+1  = diag_down_right
         return
-    
-    def CreateRegularGraph(self,size):
-        #Creates a mesh-like structure similar to a regular matrix of order equal to 'size'
+
+    def CreateRegularGraph(self, size):
+        # Creates a mesh-like structure similar to a regular matrix of order equal to 'size'
         self.__AddNodes(size)
         self.__AddEdges(size)
         self.Helper = size
         return
-    #saves the graph in the local directory and dislays the graph
+
+    # saves the graph in the local directory and dislays the graph
     def Display(self):
         # Show the plot in non-blocking mode
         plt.show(block=False)
@@ -58,10 +61,10 @@ class PathSearch(object):
         nx.draw_networkx_labels(self.X, self.nodes_dict, font_size=0)
 
         edges = self.X.edges()
-        colors = [self.X[u][v]['color'] for u,v in edges]
-        
-        nx.draw_networkx_edges(self.X, self.nodes_dict, width = 2, edge_color=colors, alpha=0.75)
-        
+        colors = [self.X[u][v]['color'] for u, v in edges]
+
+        nx.draw_networkx_edges(self.X, self.nodes_dict, width=2, edge_color=colors, alpha=0.75)
+
         if self.search_label is 1:
             plt.title('A* Search')
         elif self.search_label is 2:
@@ -70,8 +73,8 @@ class PathSearch(object):
             plt.title('No Search Performed')
         plt.savefig("Graph.pdf", format="PDF")
         plt.show()
-    
-    #Deletes nodes randomly
+
+    # Deletes nodes randomly
     def __DeleteRandom(self):
         # Delete between helper and ((helper*helper)/2)
         max_nodes = (self.Helper * self.Helper) - 1
@@ -84,15 +87,16 @@ class PathSearch(object):
             rn = random.randrange(0, max_nodes)
             if self.X.has_node(rn) is False:
                 continue
-            elif  (rn == self.START) or (rn == self.GOAL):
+            elif (rn == self.START) or (rn == self.GOAL):
                 continue
             else:
                 self.X.remove_node(rn)
-                nodes_deleted+=1
+                nodes_deleted += 1
         print("nodes deleted:", nodes_deleted)
         return
-    #Simple heuristic, determines the distance between a pair of nodes
-    def heuristic(self,a, b):
+
+    # Simple heuristic, determines the distance between a pair of nodes
+    def heuristic(self, a, b):
         aX = self.nodes_dict.get(a)[0]
         aY = self.nodes_dict.get(a)[1]
         bX = self.nodes_dict.get(b)[0]
@@ -152,9 +156,9 @@ class PathSearch(object):
         if (self.X.has_node(self.START) is False) or (self.X.has_node(self.GOAL) is False):
             print("Start or goal doesn't exist")
             return 0
-        #Delete Nodes
+        # Delete Nodes
         self.__DeleteRandom()
-        #Using priority queue
+        # Using priority queue
         Pawn = PriorityQueue()
         Pawn.put(self.START, 0)
         came_from = {}
@@ -166,25 +170,27 @@ class PathSearch(object):
             if current == self.GOAL:
                 break
             for Next in self.X.neighbors(current):
-                tentative_score = Score[current]+ self.heuristic(current, self.GOAL)
+                tentative_score = Score[current] + self.heuristic(current, self.GOAL)
                 if Next not in Score or tentative_score < Score[Next]:
                     Score[Next] = tentative_score
                     fScore = tentative_score + self.heuristic(self.GOAL, Next)
                     Pawn.put(Next, fScore)
-                    came_from[Next] = current     
-        #Now return the path
+                    came_from[Next] = current
+                    # Now return the path
         returnPath = {}
-        returner = self.GOAL        
+        returner = self.GOAL
         while returner is not self.START:
-            returnPath.update({returner:came_from[returner]})
-            #Since adding an edge that already exists updates the edge data.
-            #Change color of edges
-            self.X.add_edge(returner, came_from[returner], color = 'R')
+            returnPath.update({returner: came_from[returner]})
+            # Since adding an edge that already exists updates the edge data.
+            # Change color of edges
+            self.X.add_edge(returner, came_from[returner], color='R')
             returner = came_from[returner]
             pathSize = pathSize + 1
-        #print (returnPath.items())
+        # print (returnPath.items())
         self.search_label = 1
         return pathSize
+
+
 def main():
     A = PathSearch()
     A.CreateRegularGraph(20)
@@ -194,4 +200,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
